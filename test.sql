18cbd6c4f41c3bd14caf150423af1d1332a5ae6c
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-03-11 14:57:14 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -59,4 +59,37 @@ JOIN Programs p ON e.ProgramID = p.ProgramID;
 
 -----------------------------------join--query-------------------------ends-------------
 
+-----------------------------------Exercise-------------------------For 1NF-to-2NF-----Starts-------------
+Exercise: Normalize the Student_Courses Table to 1NF and 2NF
+Given Unnormalized Table (UNF)
+#A university stores student enrollment details in the following table:
+
+Student_Courses
++-----------+---------+----------------------+------------+
+| StudentID | Name    | Courses              | Instructors|
++-----------+---------+----------------------+------------+
+| 201       | Alice   | Math, Physics        | Dr. A, Dr. B|
+| 202       | Bob     | Chemistry            | Dr. C      |
+| 203       | Charlie | Math, Biology        | Dr. A, Dr. D|
++-----------+---------+----------------------+------------+
+Issues:
+The Courses column contains multiple values (not atomic).
+The Instructors column also contains multiple values.
+It violates the First Normal Form (1NF) rule.
+
+Step 1: Convert to First Normal Form (1NF)
+Definition of 1NF: A table is in 1NF if:
+It has a primary key.
+All columns contain atomic (indivisible) values.
+There are no repeating groups or arrays.
+
+#Definition of 2NF: A table is in 2NF if:
+It is in 1NF.
+All non-key columns are fully dependent on the whole primary key (No partial dependencies).
+Solution:
+Create a separate Students table for student information.
+Create a Student_Courses_2NF table for course enrollment details.
+
+-----------------------------------Exercise-------------------------For 1NF-to-2NF-----Ends-------------
+
 
