ddc299d4bddc385ba4e7ffbbf6beb08160b4ab2f
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-03-12 09:53:07 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -176,5 +176,20 @@ CREATE TABLE StudentCourses_3NF (
 
 
 -----------------------------------Exercise-------------------------For 1NF-to-2NF-----Ends-------------
+-----------------------------------Data Modeling Exercise: Travel Domain-----Starts-------------
+Step 1: Conceptual Model (High-Level Design)
+Entities & Relationships
+In the travel domain, key entities and their relationships can be defined as follows:
+
+Traveler: A person booking trips.
+Trip: Represents a journey taken by a traveler.
+Destination: Places where trips are made.
+Booking: A record of a trip booked by a traveler.
+Payment: Details of payments made for bookings.
+Transportation: Modes of transport (Flight, Train, Bus, etc.).
+Accommodation: Hotels or places travelers stay.
+Review: Ratings & feedback by travelers.
+
+-----------------------------------Data Modeling Exercise: Travel Domain-----Ends-------------
 
 
