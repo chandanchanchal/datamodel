f5b5221ae25b5a5ccb89588a65add9573a9c39ff
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-03-12 15:08:12 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -415,5 +415,53 @@ db.destinations.aggregate([
     }
   }
 ]);
+-----------------------------------------DWH---------------------------------------Starts----------------------
+What is Data Warehousing?
+A Data Warehouse (DWH) is a system used for reporting and data analysis. It integrates data from multiple sources and stores historical data for business intelligence.
+
+🔹 Key Characteristics of a Data Warehouse:
+✔ Subject-Oriented – Focuses on a specific business area (e.g., Travel).
+✔ Integrated – Combines data from multiple sources.
+✔ Time-Variant – Stores historical data.
+✔ Non-Volatile – Data is read-only, not updated frequently.
+
+Section 2: Travel Domain Data Warehouse
+ Business Case: Travel Data Warehouse
+A travel agency wants to analyze:
+
+Bookings & Revenue trends.
+Popular travel destinations.
+Customer behavior (frequent travelers, spending patterns).
+ Data Warehouse Schema Approach
+Fact Table: Stores measurable business transactions (e.g., Bookings).
+Dimension Tables: Store descriptive data (e.g., Traveler, Destination, Payment).
+Schema Types:
+Star Schema – Simpler structure with direct relationships.
+Snowflake Schema – Normalized structure for better data integrity.
+
+Section 3: Designing the Data Warehouse Schema
+ Step 1: Identify Fact & Dimension Tables
+Fact Table	                    Measures	                        Granularity (1 row per...)
+Fact_Booking	                Total amount, booking count	        Booking ID
+
+    
+Dimension Tables	            Attributes
+Dim_Traveler	                Traveler Name, Email, Phone
+Dim_Destination	                Destination Name, Country, City
+Dim_Payment                    	Payment Method, Payment Status
+Dim_Date	                    Date, Year, Month, Day
+
+Section 4: Implementing the Data Warehouse in MySQL (Star Schema)
+Step 2: Create the Database in DBeaver
+Open DBeaver.
+Connect to MySQL → Click New Connection → Select MySQL.
+Create a new database
+
+CREATE DATABASE TravelDWH;
+USE TravelDWH;
+
+
+
+-----------------------------------------DWH---------------------------------------Ends----------------------
 
 
