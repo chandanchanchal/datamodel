3bda84fb8c6685ab04937807ec1e5e1128c0b470
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-03-19 14:45:39 +0530

    Update play.sql

--- play.sql
+++ play.sql
@@ -219,3 +219,15 @@ Entities & Relationships
 In the travel domain, key entities and their relationships can be defined as follows:
 
 Traveler: A person booking trips.
+Trip: Represents a journey taken by a traveler.
+Destination: Places where trips are made.
+Booking: A record of a trip booked by a traveler.
+Payment: Details of payments made for bookings.
+Transportation: Modes of transport (Flight, Train, Bus, etc.).
+Accommodation: Hotels or places travelers stay.
+Review: Ratings & feedback by travelers.
+
+
+Step 2: Logical Model (Detailed Attributes & Relationships)
+Now, we define attributes and relationships.
+One traveler can have many bookings.
